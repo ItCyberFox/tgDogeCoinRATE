386c9809a32aa40975b6d03891da866496930010
ItCyberFox <79093075+ItCyberFox@users.noreply.github.com>
2021-03-16 18:24:22 +0300

    Update DOGE_COIN_TGBO.py
    добавлены BTC и ETH

--- DOGE_COIN_TGBO.py
+++ DOGE_COIN_TGBO.py
@@ -2,45 +2,97 @@ import requests
 from datetime import datetime
 import telebot
 from auth_data import token
+import traceback
 
 
-def get_data():
+def get_doge():
+    req = requests.get("https://yobit.net/api/3/ticker/doge_usd")
+    response = req.json()
+    sell_price = response["doge_usd"]["sell"]
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell DOGE price: {sell_price}")
+
+def get_btc():
     req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
     response = req.json()
     sell_price = response["btc_usd"]["sell"]
     print(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}")
 
+def get_eth():
+    req = requests.get("https://yobit.net/api/3/ticker/eth_usd")
+    response = req.json()
+    sell_price = response["eth_usd"]["sell"]
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell ETH price: {sell_price}")
 
-def telegram_bot(token):
-    bot = telebot.TeleBot(token)
 
-    @bot.message_handler(commands=["start"])
-    def start_message(message):
-        bot.send_message(message.chat.id, "Hello friend! Write the 'price' to find out the cost of BTC!")
+bot = telebot.TeleBot(token)
 
-    @bot.message_handler(content_types=["text"])
-    def send_text(message):
-        if message.text.lower() == "price":
-            try:
-                req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
-                response = req.json()
-                sell_price = response["btc_usd"]["sell"]
-                bot.send_message(
-                    message.chat.id,
-                    f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}"
-                )
-            except Exception as ex:
-                print(ex)
-                bot.send_message(
-                    message.chat.id,
-                    "Damn...Something was wrong..."
-                )
-        else:
-            bot.send_message(message.chat.id, "Whaaat??? Check the command dude!")
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    bot.send_message(message.chat.id,"Hello friend! Write the '/doge, /btc or /eth' to find out the cost of DOGE BTC OR ETH!")
 
-    bot.polling()
+
+
+@bot.message_handler(commands=["doge"])
+def senddoge_text(message):
+    try:
+        req = requests.get("https://yobit.net/api/3/ticker/doge_usd")
+        response = req.json()
+        sell_price = response["doge_usd"]["sell"]
+        bot.send_message(
+            message.chat.id,
+            f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell DOGE price: {sell_price}"
+        )
+
+
+    except Exception as e:
+        traceback.print_exc(e)
+        bot.send_message(
+            message.chat.id,
+            "Damn...Something was wrong..."
+        )
+
+@bot.message_handler(commands=["btc"])
+def sendbtc_text(message):
+        try:
+            req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
+            response = req.json()
+            sell_price = response["btc_usd"]["sell"]
+            bot.send_message(
+                message.chat.id,
+                f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}"
+            )
+
+
+        except Exception as e:
+            traceback.print_exc(e)
+            bot.send_message(
+                message.chat.id,
+                "Damn...Something was wrong..."
+            )
+
+
+@bot.message_handler(commands=["eth"])
+def sendeth_text(message):
+        try:
+            req = requests.get("https://yobit.net/api/3/ticker/eth_usd")
+            response = req.json()
+            sell_price = response["eth_usd"]["sell"]
+            bot.send_message(
+                message.chat.id,
+                f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell ETH price: {sell_price}"
+            )
+
+        except Exception as e:
+            traceback.print_exc(e)
+            bot.send_message(
+                message.chat.id,
+                "Damn...Something was wrong..."
+            )
+
 
 
 if __name__ == '__main__':
-    # get_data()
-    telegram_bot(token)
+    try:
+        bot.polling(none_stop=True)
+    except:
+        pass
